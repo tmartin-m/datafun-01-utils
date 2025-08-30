0c61c4d7626b5cb8c650becb582020e3f37b27f3
Taylor Martin <tmmartin915@gmail.com>
2025-08-29 19:43:46 -0500

    Finish TODOs

--- utils_taylorm.py
+++ utils_taylorm.py
@@ -54,25 +54,22 @@ logger.info("Logger loaded.")
 # ----------------------------------
 is_accepting_clients: bool = True
 offers_remote_workshops: bool = True
-
-# TODO: Declare and initialize a new boolean variable (e.g. `is_hiring` and set it to False)
+is_hiring: bool = False
 
 # ----------------------------------
 # Define Integer variables
 # ----------------------------------
 current_year: int = 2025
 year_started: int = 2020
-
-# TODO: Declare and initialize a new integer variable (e.g. `number_of_employees` and set it to 25)
+number_of_employees: int = 25
 
 # ----------------------------------
 # Define String variables
 # ----------------------------------
-author: str = "Denise Case"  # TODO: change to your name
-organization: str = "Stellar Analytics"  # TODO: change the org name
-motto: str = "Clear. Useful. On time."  # TODO: change the motto
-
-# TODO: Declare and initialize a new string variable (e.g. `location` and set it to your city and state)
+author: str = "Taylor Martin"  
+organization: str = "Martin Analytics"  
+motto: str = "Trying our best, to make you the best"  
+location: str = "Moline, IL"
 
 # ----------------------------------
 # Define List variables
@@ -82,10 +79,7 @@ services: list[str] = ["Data Analysis", "Machine Learning", "Business Intelligen
 
 # example list of floating point numbers
 satisfaction_scores: list[float] = [4.8, 4.6, 4.9, 5.0, 4.7]
-
-
-# TODO: Declare and initialize a new list variable (e.g. `office_locations` and set it to 3-5 cities where your org has offices)
-# TODO: Wrap each string in quotes and separate each item with a comma.
+office_locations: list[float] = ["St Louis", "Chicago", "Des Moines", "Milwaukee"]
 
 # ----------------------------------
 # Use built-in Python operators (such as - + * /)
@@ -96,9 +90,7 @@ min_score: float = min(satisfaction_scores)
 max_score: float = max(satisfaction_scores)
 count_of_services: int = len(services)
 count_of_scores: int = len(satisfaction_scores)
-
-# TODO: Declare and initialize a new calculated variable (e.g. `count_of_locations` that uses the len() function on your list of office locations)
-
+count_of_locations: int = len(office_locations)
 
 # ----------------------------------
 # Use the built-in statistics module functions (such as mean, stdev, etc.)
@@ -116,7 +108,12 @@ byline: str = f"""
 Author:                     {author}
 Motto:                      {motto}
 Years Active:               {years_active}
-Accepting New Clients?:     {is_accepting_clients}
+Main Office:                {location}
+Number of Locations:        {count_of_locations}
+Other Office Locations:     {office_locations}
+Employees?:                 {number_of_employees}
+Hiring?:                    {is_hiring}
+New Clients?:               {is_accepting_clients}
 Remote Workshops?:          {offers_remote_workshops}
 Services:                   {services}
 Client Satisfaction Scores: {satisfaction_scores}
@@ -126,8 +123,6 @@ Mean Satisfaction Score:    {mean_score:.2f}
      Standard Deviation:    {stdev_score:.2f}
 **********************************************************
 """
-
-# TODO: Modify the byline f-string to display your new variables as well. Use curly braces {} to embed each variable.
 
 #####################################
 # Define Global Functions
@@ -166,8 +161,7 @@ def main() -> None:
     loguru.logger.info("Byline:\n" + get_byline())
 
     try:
-        # TODO: Uncomment next line if you want audio feedback (use CTRL+C to stop)
-        # read_byline_aloud()
+        read_byline_aloud()
         pass
     except KeyboardInterrupt:
         logger.info("Speech interrupted by user (Ctrl+C).")
